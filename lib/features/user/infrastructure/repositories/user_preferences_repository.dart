72e2c5d5e07445e6a6cdc821449a90a99239aaf7
Walden <iverson.wuwei@gmail.com>
2026-02-14 00:04:46 +0800

    ``` refactor(user): 调整dio导入顺序
    将dio包的导入语句移动到文件顶部，保持与其他导入语句的一致性，
    提高代码可读性和维护性。
    ```

--- lib/features/user/infrastructure/repositories/user_preferences_repository.dart
+++ lib/features/user/infrastructure/repositories/user_preferences_repository.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:go_nomads_app/config/api_config.dart';
 import 'package:go_nomads_app/features/user/domain/entities/user_preferences.dart';
 import 'package:go_nomads_app/features/user/domain/repositories/i_user_preferences_repository.dart';
 import 'package:go_nomads_app/services/token_storage_service.dart';
-import 'package:dio/dio.dart';
 
 /// 用户偏好设置仓储实现
 class UserPreferencesRepository implements IUserPreferencesRepository {
